8b24a62c1883123333150a37ceb7059c3782b69f
unknown <whitebeard086@gmail.com>
2022-06-03 05:41:25 +0100

    dynamically show or hide sidebar according to screen size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,16 +19,41 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       className="relative text-xl rounded-full p-3 hover:bg-light-gray">
       <span
         style={{ background: dotColor }}
-        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-        {icon}
-      </span>
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      />
+      {icon}
     </button>
   </TooltipComponent>
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext();
+  const {
+    activeMenu,
+    setActiveMenu,
+    isClicked,
+    setIsClicked,
+    handleClick,
+    screenSize,
+    setScreenSize,
+  } = useStateContext();
 
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
@@ -61,10 +86,12 @@ const Navbar = () => {
           icon={<RiNotification3Line />}
         />
         <TooltipComponent content="Profile" position="BottomCenter">
-          <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={() => handleClick("userProfile")}>
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("userProfile")}>
             <img src={avatar} alt="user avatar" className="rounded-full w-8 h-8" />
             <p>
-              <span className="text-gray-400 text-14">Hi, </span> {" "}
+              <span className="text-gray-400 text-14">Hi, </span>{" "}
               <span className="text-gray-400 font-bold ml-1 text-14">Alex</span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
