5bd3fd5cbcc50b9e8ea6a183a338b51d005a06dc
unknown <whitebeard086@gmail.com>
2022-06-04 09:25:50 +0100

    ecommerce page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,12 @@
-const Button = () => {
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
   return (
-    <div>Button</div>
-  )
-}
-export default Button
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
+  );
+};
+export default Button;
--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,6 +1,29 @@
-const SparkLine = () => {
+import React from "react";
+import { SparklineComponent, Inject, SparklineTooltip } from "@syncfusion/ej2-react-charts";
+
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
   return (
-    <div>SparkLine</div>
-  )
-}
-export default SparkLine
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="yval"
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        format: "${x} : data ${yval}",
+        tracklineSettings: {
+          visible: true,
+        },
+      }}>
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
+};
+export default SparkLine;
