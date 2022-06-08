a2efe4f6b939d75a7befc879aeb079030bdcc6c8
unknown <whitebeard086@gmail.com>
2022-06-08 10:30:41 +0100

    calendar component

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
                 {/* Apps */}
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
-                <Route path="/calender" element={<Calendar />} />
+                <Route path="/calendar" element={<Calendar />} />
                 <Route path="/color-picker" element={<ColorPicker />} />
 
                 {/* Charts */}
