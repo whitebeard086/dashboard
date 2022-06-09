099b326b5f8403a7eed1c7191a5887dbc5f13bb3
unknown <whitebeard086@gmail.com>
2022-06-09 22:24:27 +0100

    kanban page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,4 @@
-import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from "@syncfusion/ej2-react-schedule";
-import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from "@syncfusion/ej2-react-schedule";
 
 import { scheduleData } from "../data/dummy";
 import { Header } from "../components"
