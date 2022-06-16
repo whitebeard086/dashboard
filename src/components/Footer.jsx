4f050b9ce4f7fda8e81d3ff28fc3791dea8cd32b
Mr. Alex <whitebeard086@gmail.com>
2022-06-16 16:36:05 +0100

    user profile, chat, and footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
-export default Footer
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2022 All rights reserved by Alex
+    </p>
+  </div>
+);
+
+export default Footer;
