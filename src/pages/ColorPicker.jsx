b1baf9982f1e9687b63b0522ce72a984e2949539
Mr. Alex <whitebeard086@gmail.com>
2022-06-16 05:49:13 +0100

    color picker

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -2,6 +2,10 @@ import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs"
 
 import { Header } from "../components"
 
+const change = (args) => {
+  document.getElementById("preview").style.backgroundColor = args.currentValue.hex;
+}
+
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -9,7 +13,7 @@ const ColorPicker = () => {
       <div className="text-center">
         <div id="preview" />
         <div className="flex justify-center items-center gap-20 flex-wrap">
-          <div>
+          <div className="mr-5">
             <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
             <ColorPickerComponent 
               id="inline-pallete"
@@ -17,6 +21,18 @@ const ColorPicker = () => {
               modeSwitcher={false} 
               inline
               showButtons={false}
+              change={change}
+            />
+          </div>
+          <div>
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+            <ColorPickerComponent 
+              id="inline-pallete"
+              mode="Picker"
+              modeSwitcher={false} 
+              inline
+              showButtons={false}
+              change={change}
             />
           </div>
         </div>
